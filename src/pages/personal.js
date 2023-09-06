89ba9903e50e7d46b477b92c5558f4bdb9bec214
LAREINA-JO <rui.guan@ucalgary.ca>
2023-09-05 20:12:14 -0400

    change

---
+++ src/pages/personal.js
@@ -0,0 +1,40 @@
+// import Image from 'next/image'
+import { Inter } from "next/font/google";
+import Link from "next/link";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
+import EmailIcon from '@mui/icons-material/Email';
+import Head from "next/head";
+import * as React from 'react';
+import Divider from '@mui/material/Divider';
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+
+
+
+{/* 
+  Underline Color:
+    Name: slate-400
+    Degree: lime-400
+    School: orange-400
+    Interest Direction: sky-400
+
+  Text Color:
+    Skills: violet-800
+*/}
+
+const inter = Inter({ subsets: ["latin"] });
+
+
+export default function Home() {
+
+  return (
+    <></>
+  );
+}
